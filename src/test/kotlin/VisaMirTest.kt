0b39b53e5334a8cae0b5191ac93affa82c6f0754
sinoptik13rus <set.91@mail.ru>
2022-03-21 16:46:26 +0300

    Add ERROR in test

--- src/test/kotlin/VisaMirTest.kt
+++ src/test/kotlin/VisaMirTest.kt
@@ -7,46 +7,46 @@ class VisaMirTest {
     @Test
     fun visaMir() {
         val amount = 10000
-        val expectedComission = 3500.00
+        val expectedComission = 3500
 
-        val actualComission = visaMir(amount)
-        expectedComission == actualComission
+        val actualComission = (visaMir(amount)).toInt()
+        assertEquals(expectedComission, actualComission)
     }
 
     @Test
     fun visaMir1() {
         val amount = 10000000
-        val expectedComission = 75000.00
+        val expectedComission = 75000
 
-        val actualComission = visaMir(amount)
-        expectedComission == actualComission
+        val actualComission = (visaMir(amount)).toInt()
+        assertEquals(expectedComission, actualComission)
     }
 
 
     @Test
     fun visaMir2() {
         val amount = 100_000_000
-        val expectedComission = -1.00
+        val expectedComission = -1
 
-        val actualComission = visaMir(amount)
-        expectedComission == actualComission
+        val actualComission = (visaMir(amount)).toInt()
+        assertEquals(expectedComission, actualComission)
     }
 
     @Test
     fun visaMir4() {
         val amount = 10000
-        val expectedComission = 3500.00
+        val expectedComission = 3500
 
-        val actualComission = mastercardMaestro(amount)
-        expectedComission == actualComission
+        val actualComission = (visaMir(amount)).toInt()
+        assertEquals(expectedComission, actualComission)
     }
 
     @Test
     fun visaMir5() {
         val amount = 10000000
-        val expectedComission = 75000.00
+        val expectedComission = 75000
 
-        val actualComission = mastercardMaestro(amount)
-        expectedComission == actualComission
+        val actualComission = (mastercardMaestro(amount)).toInt()
+        assertEquals(expectedComission, actualComission)
     }
 }
